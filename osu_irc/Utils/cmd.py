8f9bf2cff9ced4235f4427d2a82a8c30982e5592
The-CJ <christoperjaekel@googlemail.com>
2019-08-25 18:11:34 +0200

    moved cmds

---
+++ osu_irc/Utils/cmd.py
@@ -0,0 +1,12 @@
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..Classes.client import Client
+
+async def sendPong(cls:"Client"):
+	await cls.sendContent( "PONG :cho.ppy.sh\r\n", ignore_limit=True )
+
+async def sendNick(cls:"Client"):
+	await cls.sendContent( f"NICK {cls.nickname}\r\n", ignore_limit=True )
+
+async def sendPass(cls:"Client"):
+	await cls.sendContent( f"PASS {cls.token}\r\n", ignore_limit=True )
