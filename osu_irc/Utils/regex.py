c1ec7ba411ae377225025382f282169fb2992f62
The_CJ <christoperjaekel@googlemail.com>
2020-09-16 19:48:50 +0200

    user and regex rebuild

--- osu_irc/Utils/regex.py
+++ osu_irc/Utils/regex.py
@@ -1,5 +1,6 @@
 import re
 
+# IRC events
 RePing:"re.Pattern" = re.compile(r"^PING.*")
 ReOnReady:"re.Pattern" = re.compile(r"^:cho\.ppy\.sh 001.*")
 ReGarbage:"re.Pattern" = re.compile(r"^.*cho\.ppy\.sh (366|375|372|376).*")
@@ -10,5 +11,10 @@ ReQuit:"re.Pattern" = re.compile(r"^:(.+?)!cho\@ppy\.sh QUIT :(.+?)")
 ReUserList:"re.Pattern" = re.compile(r"^:cho\.ppy\.sh 353.*")
 RePrivMessage:"re.Pattern" = re.compile(r"^:(.+?)!cho\@ppy\.sh PRIVMSG .+? :.*")
 
+# extended IRC Events
 ReUserListData:"re.Pattern" = re.compile(r".*353 .* = #(\S+?) :(.*)$")
 ReAction:"re.Pattern" = re.compile(r"\x01{1}ACTION (.+?)\x01{1}")
+
+# other
+ReUserName:"re.Pattern" = re.compile(r"(?:@|;| |^):(\S*?)!cho@ppy.sh[; ]")
+ReRoomName:"re.Pattern" = re.compile(r"[@; ](?:JOIN|PART|CLEARMSG|CLEARCHAT|ROOMSTATE|USERSTATE|PRIVMSG|USERNOTICE) :#(\S*?)(?:[; ]|$)")
