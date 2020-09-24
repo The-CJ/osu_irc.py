c1e0535d647766dc0e5a0c46bd6cd6061b3a0e82
The_CJ <christoperjaekel@googlemail.com>
2020-09-24 18:54:15 +0200

    a bit error handling

--- osu_irc/Utils/commands.py
+++ osu_irc/Utils/commands.py
@@ -11,6 +11,8 @@ Log:logging.Logger = logging.getLogger("osu_irc")
 async def sendMessage(cls:"Client", Chan:Channel or str, content:str):
 	if isinstance(Chan, Channel):
 		destination:str = Chan.name
+	elif isinstance(Chan, User):
+		raise ValueError(f"sendMessage() is meant for channels only, please use sendPM() for messages to a user")
 	else:
 		destination:str = str(Chan)
 
@@ -21,6 +23,8 @@ async def sendMessage(cls:"Client", Chan:Channel or str, content:str):
 async def sendPM(cls:"Client", Us:User or str, content:str):
 	if isinstance(Us, User):
 		destination:str = Us.name
+	elif isinstance(Us, Channel):
+		raise ValueError(f"sendPM() is meant for users only, please use sendMessage() for messages to a channel")
 	else:
 		destination:str = str(Us)
 
@@ -31,6 +35,8 @@ async def sendPM(cls:"Client", Us:User or str, content:str):
 async def joinChannel(cls:"Client", Chan:Channel or str):
 	if isinstance(Chan, Channel):
 		destination:str = Chan.name
+	elif isinstance(Chan, User):
+		raise ValueError(f"you can not join a user, just start PM-ing, duh")
 	else:
 		destination:str = str(Chan)
 
@@ -41,6 +47,8 @@ async def joinChannel(cls:"Client", Chan:Channel or str):
 async def partChannel(cls:"Client", Chan:Channel or str):
 	if isinstance(Chan, Channel):
 		destination:str = Chan.name
+	elif isinstance(Chan, User):
+		raise ValueError(f"you can not part a user, its not a channel, duh")
 	else:
 		destination:str = str(Chan)
 
